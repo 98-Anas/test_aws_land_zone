79e9423355c63d8e127e624f2026078457745e6f
98-Anas <zonzo@Anas-PU.localdomain>
2025-06-29 12:17:48 +0300

    pre-production deploy

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = module.networking.vpc_id
+  value       = modules.networking.vpc_id
 }
 
 output "instance_public_ips" {
   description = "Public IP addresses of EC2 instances"
-  value       = module.compute.instance_public_ips
+  value       = modules.compute.instance_public_ips
 }
 
 output "cloudwatch_log_group_name" {
   description = "Name of the CloudWatch log group"
-  value       = module.logging.cloudwatch_log_group_name
+  value       = modules.logging.cloudwatch_log_group_name
 }
